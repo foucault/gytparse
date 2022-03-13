f8e66c048fd674eb59a175c33d5f5828c7c9ac68
Spyros Stathopoulos <spystath@gmail.com>
2022-03-13 17:13:50 +0000

    Add a function to generate current proxy URL from settings

--- gytparse/settings.py
+++ gytparse/settings.py
@@ -1,4 +1,25 @@
+import urllib
+from types import MethodType
 from gi.repository import Gio
 
 
-Settings = Gio.Settings.new('gr.oscillate.gytparse')
+def __current_proxy_url(slf):
+    proxy_type = slf.get_string('proxy-type')
+
+    if proxy_type == 'none':
+        return None
+
+    proxy_host = slf.get_string('proxy-host').encode('idna').decode()
+    proxy_port = slf.get_int('proxy-port')
+
+    return "%s://%s:%d" % (proxy_type, proxy_host, proxy_port)
+
+
+def __make_settings(resource='gr.oscillate.gytparse'):
+    settings = Gio.Settings.new(resource)
+    settings.proxy_url = MethodType(__current_proxy_url, settings)
+
+    return settings
+
+
+Settings = __make_settings('gr.oscillate.gytparse')
