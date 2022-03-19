a2a6f45e3620adb5b449cc9e1098e94b1badb98b
Spyros Stathopoulos <spystath@gmail.com>
2022-03-19 20:45:26 +0000

    Cleanup all `meson.build`s and move gsettings schema into `data`

--- localrun.sh
+++ localrun.sh
@@ -3,7 +3,7 @@
 OVERLAY="/gr/oscillate/gytparse=$(pwd)/data"
 PYTHONPATH="$(pwd):${PYTHONPATH}"
 
-glib-compile-schemas gytparse --targetdir=$(pwd)
+glib-compile-schemas data --targetdir=$(pwd)
 
 GSETTINGS_SCHEMA_DIR="$(pwd):${GSETTINGS_SCHEMA_DIR}" \
 G_RESOURCE_OVERLAYS=${OVERLAY} \
