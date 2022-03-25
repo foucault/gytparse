c0362e79c9747e3baad1a81b0381a2bacb07e4e1
Spyros Stathopoulos <spystath@gmail.com>
2022-03-25 18:48:02 +0000

    Use external ytdl instance to download videos
    Unfortunately the ytdl library does not give us an option to cancel
    downloads in-progress programmatically. As such the download operation
    now forks to a background ytdl binary. Progress tracking is done via
    custom progress templates and reading ytdl's stdout. Cancelling the
    download will amount to sending SIGINT and reaping the child.

--- gytparse/operation.py
+++ gytparse/operation.py
@@ -1,6 +1,10 @@
 import sys
+import os
 import os.path
+import signal
 import requests
+import shutil
+import time
 from functools import partial
 from . import youtube
 
@@ -10,8 +14,10 @@ from .settings import Settings
 
 try:
     import yt_dlp as youtube_dl
+    YTDLNAME = 'yt-dlp'
 except ModuleNotFoundError:
     import youtube_dl
+    YTDLNAME = 'youtube-dl'
 
 
 _FORMAT_TMPL = 'bestvideo[height<=?%d]+bestaudio/best[height<=?%d]'
@@ -49,56 +55,96 @@ class VideoFetcher(GObject.GObject):
         self.running = False
 
     def queue_video(self, url, target):
-        self.videos.append({'url': url, 'target': target, 'offset': 0, 'totalsize': 0})
+        self.videos.append(\
+            {'url': url, 'target': target, 'offset': 0, 'totalsize': 0, \
+             'running': False, 'pid': -1})
 
     def unqueue_video(self, url):
-        # how ??
-        pass
+        for v in self.videos:
+            if v['url'] == url:
+                self.__kill_process(v)
+
+    def __extract_filesize(self, url):
+        ydl = youtube_dl.YoutubeDL({
+            'format': ytdl_fmt_from_str(Settings.get_string('download-quality')),
+            'logger': _VideoFetchLogger()})
+        result = ydl.extract_info(url, download=False)
+        totalfilesize = 0
+        for fmt in result['requested_formats']:
+            totalfilesize += fmt['filesize']
+
+        return totalfilesize
+
+    def __reap_child(self, pid, status, video):
+        print('Reapping PID', pid, 'status:', status)
+        GLib.spawn_close_pid(pid)
+        self.video_finished.emit(video['url'])
+        video['running'] = False
+        self.videos.pop(0)
+
+    def __kill_process(self, video):
+        print('Killing PID', video['pid'])
+        pid = video['pid']
+        if pid > 0:
+            os.kill(pid, signal.SIGINT)
 
     def __fetch_inner(self, video):
         url = video['url']
         target = video['target']
 
-        def hook(data, video):
-            if data['status'] == 'downloading':
-                total_so_far = video['offset'] + data['downloaded_bytes']
-                self.video_progress.emit(video['url'], total_so_far, video['totalsize'])
-            elif data['status'] == 'finished':
-                video['offset'] = data['downloaded_bytes']
+        ytdl = shutil.which(YTDLNAME)
+        if ytdl is None:
+            return
 
-        opts = {
-            'outtmpl': os.path.join(target, '%(title)s.%(ext)s'),
-            'format': ytdl_fmt_from_str(Settings.get_string('download-quality')),
-            'progress_hooks': [partial(hook, video=video)],
-            'logger': _VideoFetchLogger()
-        }
+        fmt = ytdl_fmt_from_str(Settings.get_string('download-quality'))
+        proxy = Settings.proxy_url()
+        flags = GLib.SPAWN_DO_NOT_REAP_CHILD | GLib.SPAWN_STDERR_TO_DEV_NULL
+        video['totalsize'] = self.__extract_filesize(url)
 
-        if Settings.proxy_url() is not None:
-            opts['proxy'] = Settings.proxy_url()
-
+        cmd = [ytdl]
+        if proxy is not None:
+            cmd.append('--proxy=%s' % proxy)
+        cmd.append('--quiet')
+        cmd.append('--format=%s' % fmt)
+        cmd.append('--progress')
+        cmd.append('--progress-template=%(progress.status)s;;%(progress.downloaded_bytes)d')
         if Settings.get_string('output-merge-format') != 'automatic':
-            opts['merge_output_format'] = Settings.get_string('output-merge-format')
-
-        self.ydl = youtube_dl.YoutubeDL(opts)
-        result = self.ydl.extract_info(url, download=False)
-        totalfilesize = 0
-        for fmt in result['requested_formats']:
-            totalfilesize += fmt['filesize']
-
-        video['totalsize'] = totalfilesize
+            cmd.append("--merge-output-format=%s" % Settings.get_string('output-merge-format'))
+        cmd.append('--output='+ os.path.join(target, '%(title)s.%(ext)s'))
+        cmd.append(video['url'])
 
         self.video_starting.emit(url, video['totalsize'])
+        video['running'] = True
+        (pid, _, cstdout, _) = \
+            GLib.spawn_async(cmd, standard_output=True, standard_error=False, flags=flags)
+        video['pid'] = pid
+        GLib.child_watch_add(\
+            GLib.PRIORITY_DEFAULT_IDLE, pid, \
+            partial(self.__reap_child, video=video))
 
-        self.ydl.download([url])
-
-        self.video_finished.emit(url)
-        self.ydl = None
+        with os.fdopen(cstdout, 'r') as fh:
+            while video['running']:
+                if video['offset'] >= video['totalsize']:
+                    # download has finished and video is muxing
+                    # wait for ytdl to exit
+                    time.sleep(0.1)
+                    continue
+                try:
+                    (status, received) = fh.readline().strip().split(';;')
+                except ValueError:
+                    continue
+                received = int(received)
+                if status == 'downloading':
+                    total_so_far = video['offset'] + received
+                    self.video_progress.emit(video['url'], total_so_far, video['totalsize'])
+                elif status == 'finished':
+                    video['offset'] += received
 
     def fetch(self):
 
         self.running = True
         while len(self.videos) > 0:
-            video = self.videos.pop(0)
+            video = self.videos[0]
             self.__fetch_inner(video)
         self.running = False
 
